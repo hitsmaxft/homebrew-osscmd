724a8819316df222a8fd21aca0c8589b347281c2
麒翔 <qixiang.mft@taobao.com>
2016-06-14 12:57:27 +0800

    init

---
+++ oss.rb
@@ -0,0 +1,7 @@
+class Wget < Formula
+    homepage "https://help.aliyun.com/document_detail/32184.html"
+    url "https://docs-aliyun.cn-hangzhou.oss.aliyun-inc.com/internal/oss/0.0.4/assets/sdk/OSS_Python_API_20160419.zip?spm=5176.doc32171.2.2.VA6ybX&file=OSS_Python_API_20160419.zip"
+    sha256 "cdab0be35c8d4f6f01a094ff60555e6c68e38e7e2e36ec89fc38c9d982c7847c"
+    def install
+    end
+end
