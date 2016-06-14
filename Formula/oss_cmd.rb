4b96a466b61190c7b742ae5a6bde83a2624e0cda
麒翔 <qixiang.mft@taobao.com>
2016-06-14 13:56:03 +0800

    update

---
+++ Formula/oss_cmd.rb
@@ -0,0 +1,35 @@
+# Documentation: https://github.com/Homebrew/brew/blob/master/share/doc/homebrew/Formula-Cookbook.md
+#                http://www.rubydoc.info/github/Homebrew/brew/master/Formula
+# PLEASE REMOVE ALL GENERATED COMMENTS BEFORE SUBMITTING YOUR PULL REQUEST!
+
+class OssCmd < Formula
+  desc ""
+  homepage ""
+  url "https://docs-aliyun.cn-hangzhou.oss.aliyun-inc.com/internal/oss/0.0.4/assets/sdk/OSS_Python_API_20160419.zip"
+  version "20160419"
+  sha256 "cdab0be35c8d4f6f01a094ff60555e6c68e38e7e2e36ec89fc38c9d982c7847c"
+
+  # depends_on "cmake" => :build
+  #depends_on "python" # if your formula requires any X11/XQuartz components
+
+  def install
+    # ENV.deparallelize  # if your formula fails when building in parallel
+
+    # Remove unrecognized options if warned by configure
+    # prefix.install Dir["*"]
+    system "python", *Language::Python.setup_install_args(prefix)
+    bin.install Dir["osscmd"]
+  end
+
+  test do
+    # `test do` will create, run in and delete a temporary directory.
+    #
+    # This test will fail and we won't accept that! It's enough to just replace
+    # "false" with the main program this formula installs, but it'd be nice if you
+    # were more thorough. Run the test with `brew test OSS_Python_API`. Options passed
+    # to `brew install` such as `--HEAD` also need to be provided to `brew test`.
+    #
+    # The installed folder is not in the path, so use the entire path to any
+    # executables being tested: `system "#{bin}/program", "do", "something"`.
+  end
+end
